90ccdcbcd4bcb165836b8d3828ac297e6c00594a
180133517@stu.vtc.edu.hk <180133517@stu.vtc.edu.hk>
2022-04-03 00:12:34 +0800

    fix buildspec json, added enviroment variable,add shell script to create json

--- make_json.sh
+++ make_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-IMAGE_NAME=${Container_Name}
 REPOSITORY_URI=${REPO_ECR}
-printf '[{"name": "'"$IMAGE_NAME"'","imageUri": "'"$REPOSITORY_URI"'"}]' >imagedefinitions.json
+printf '[{"name": "WebContainer","imageUri": "'"$REPOSITORY_URI"'"}]' >imagedefinitions.json
